cb58e359e03b2e3fae0b4cec41ba09bdefc228e9
Ibrahim Alfaifi <Ibrahim.farhan.ruh.k@tuwaiq.edu.sa>
2021-10-12 15:03:30 +0300

    code without change

---
+++ src/main.kt
@@ -0,0 +1,12 @@
+const val apples = 50.0
+
+fun main (){
+
+
+    val noOfDozens = apples / 12.0
+    val remainingApples = apples % 12.0
+
+    println("Number of dozens = $noOfDozens\nNumber of remaining = $remainingApples")
+
+
+}
