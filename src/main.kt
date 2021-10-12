42d83bf65567f4fb2a4ef303be21defe2778497d
Ibrahim Alfaifi <Ibrahim.farhan.ruh.k@tuwaiq.edu.sa>
2021-10-12 15:05:44 +0300

    1st change

--- src/main.kt
+++ src/main.kt
@@ -6,7 +6,7 @@ fun main (){
     val noOfDozens = apples / 12.0
     val remainingApples = apples % 12.0
 
-    println("Number of dozens = $noOfDozens\nNumber of remaining = $remainingApples")
+    println("Number of dozens is $noOfDozens\nNumber of remaining is $remainingApples")
 
 
 }
